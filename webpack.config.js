3e7abe6edd33b029d850d1a34aabbbb39f2e65e6
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-19 11:39:42 +0100

    sets up src helpers

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const config = {
  entry: `${__dirname}/client/src/app.js`,
  output: {
    path: `${__dirname}/client/public/js`,
-   filename: ‘bundle.js’
+   filename: 'bundle.js'
  },
- mode: ‘development’
+ mode: 'development'
 };
 
 module.exports = config;
